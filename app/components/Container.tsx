8dd2c297a9388a155fe4e0a90c20037a2b4305f9
linow1008 <jce951008@naver.com>
2024-03-21 23:17:54 +0900

    3/21 auth modal까지

---
+++ app/components/Container.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+const Container = ({ children }: Props) => {
+    return (
+        <div>
+            <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">{children}</div>
+        </div>
+    );
+};
+
+export default Container;
